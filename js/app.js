b9fd5d2e0344e38df8b4d8f9a4aed9ae5668fc4d
CruzMayra <velazquez.leonora@gmail.com>
2018-05-23 01:06:30 -0500

    iniciando con nueva información

---
+++ js/app.js
@@ -0,0 +1,97 @@
+/*---------- función que centraliza al resto de las funciones ----------*/
+function loadPage() {
+  $('.load-landing').click(hideHome);
+ }
+
+ const hideHome = (e) => {
+   e.preventDefault();
+   $('#home').removeClass('show');
+   $('#home').addClass('hide');
+   showPortfolio();
+ }
+
+ const showPortfolio = () => {
+  $('#landing').removeClass('hide');
+  $('#landing').addClass('show');
+ }
+
+ $('.view').click(function(e){
+  e.preventDefault();
+  const pageRef = e.target.getAttribute('href');
+  page(pageRef);
+})
+
+page('/venus-ecommerce', e => {
+  getData(e.path);
+})
+
+page('/pokedex', e => {
+  getData(e.path);
+})
+
+page('/ticket-hack', e => {
+  getData(e.path);
+})
+
+page('/scribere', e => {
+  getData(e.path);
+})
+
+page('/labcar', e => {
+  getData(e.path);
+})
+
+ function getData(dataProject) {
+   const project = projects.find( project => {
+     return project.id === dataProject;
+   })
+   paintProject(project);
+ }
+
+ function paintProject(project) {
+   $('#content-me').empty();
+   let templateProject = '';
+   templateProject += `<section id="projects">
+   <section class="jumbotron">
+     <div class="container text-center">
+       <h2 class="name">${project.name}</h2>
+       <img src="${project.image[0]}" alt="">   
+     </div> 
+   </section>
+ 
+   <section class="container project-d">
+     <div class="row">
+       <div class="col-md-9">
+         <h3>Descripción del proyecto</h3>
+         <p>${project.description}</p>
+         <img src="${project.image[1]}" alt="">         
+       </div>
+       <div class="col-md-3 details-project">
+         <div>
+           <h4>Tipo de proyecto</h4>
+           <p>${project.type}</p>
+         </div>
+         <div>
+           <h4>Qué hice :)</h4>
+           <p>${project.do}</p>
+         </div>
+         <div>
+           <h4>Tecnologías usadas</h4>
+           <p>${project.technology}</p>
+         </div>
+         <div>
+           <h4>Ver el proyecto</h4>
+           <p><a href="${project.demo}"><i class="fas fa-eye"></i></a></p>
+           <h4>Ver código</h4>
+           <p><a href="${project.code}"><i class="fab fa-github-alt"></i></a></p>
+         </div>
+       </div>
+     </div>
+   </section>
+ </section>`;
+ $('#content-me').append(templateProject); 
+ }
+
+ $(document).ready(loadPage);
+
+ page.start({ hashbang: true })
---
+++ js/data.js
@@ -0,0 +1,57 @@
+const projects = [
+  {
+    id:'/venus-ecommerce',
+    name: 'Venus e-commerce',
+    image: ['assets/images/venus-desktop-01.jpg', 'assets/images/venus-desktop-02.jpg'],
+    description: 'E-commerce dirigo a mujeres jóvenes que disfrutan de un estilo en tendencia así como de las prendas sencillas y cómodas. <br>Proyecto colaborativo que aprovecha el uso de la técnica SPA para proveer una interfaz sencilla e intuitiva, permite al usuario saber siempre en que sección se encuentra gracias al uso del routing. Aprovecha el uso de API para ofrecer una extensa variedad de productos (Etsy) y un método de pago sencillo (PayPal).',
+    type: 'E-commerce',
+    do: 'Creación de carrito de compras utilizando local Storage y manejo de routing.',
+    technology:'API, SPA, JS, Firebase, Materialize',
+    demo: 'https://cruzmayra.github.io/venus-e-commerce/',
+    code: 'https://github.com/CruzMayra/venus-e-commerce'
+  },
+  {
+    id:'/pokedex',
+    name: 'Pokédex',
+    image: ['assets/images/pokedex-desktop-01.jpg', 'assets/images/pokedex-desktop-02.jpg'],
+    description: 'Aplicación que emula la enciclopedia virtual que los entrenadores llevan consigo en la serie, para consultar las fichas de las diversas especies Pokémon.<br>Proyecto individual que hace uso de una GrahpQL API para realizar una sola petición por medio de jQuery. Aprovecha el manejo del DOM para crear de manera dinámina los modales con la información particular de cada pokémon.',
+    type: 'Ejercicio práctico del consumo de una API',
+    do: 'Consumo de API y manejo del DOM con jQuery.',
+    technology: 'API, jQuery, DOM',
+    demo: 'https://cruzmayra.github.io/pokedex/',
+    code: 'https://github.com/CruzMayra/pokedex'
+  },
+  {
+    id: '/ticket-hack',
+    name: 'Ticket-hack',
+    image: ['assets/images/phone-ticket-01.jpg', 'assets/images/phone-ticket-02.jpg'],
+    description: 'Plataforma que busca ofrecer un espacio para el intercambio de boletos, principalmente para conciertos, desde una comunidad segura y confiable.<br>Ticket-hack es un proyecto colaborativo realizado durante la hackatón interna de Laboratoria. Utiliza Firebase como medio de autentificación e identificación del usuario. Hace uso de la API de Ticketmaster para mostrar los eventos más próximos en la ciudad.',
+    type: 'Red social',
+    do: 'Autentificación de usuario, creación de base de datos de usuarios y publicaciones en Firebase.',
+    technology: 'API, Firebase, Database',
+    demo: 'https://cruzmayra.github.io/ticket-hack/',
+    code: 'https://github.com/CruzMayra/ticket-hack'
+  },
+  {
+    id: '/scribere',
+    name: 'Scribere: Write free, be free!!!',
+    image: ['assets/images/scribere-desktop-01.jpg', 'assets/images/scribere-desktop-02.jpg'],
+    description: 'Red social para los entusiastas de la escritura. Scribere permite al usuario crear sus propias historias, leer las aportaciones de otros usuarios y guardar como favoritas aquellas historias que más lo impactaron.<br>Proyecto colaborativo que utiliza el feedback de los usuarios para su creación a fin de satisfacer sus necesidades. Scribere también hace uso de Firebase como medio de autentificación e identificación de los usuarios.',
+    type: 'Red social',
+    do: 'Autentificación de usuario, creación de base de datos de usuarios e interactividad de publicaciones.',
+    technology: 'Social network, Firebase, Bootstrap',
+    demo: 'https://cruzmayra.github.io/scribere-social-network/',
+    code: 'https://github.com/CruzMayra/scribere-social-network'
+  },
+  {
+    id: '/labcar',
+    name: 'LabCar',
+    image: ['assets/images/lc-desktop-01.jpg', 'assets/images/lc-mobile-01.jpg'],
+    description: 'Proyecto individual que replica la página de Lab-Car con la utilización, en ese momento, por primera vez de un framework (Bootstrap) y la aplicación del responsive design.',
+    type: 'Práctica con framework de CSS',
+    do: 'Maquetación del sitio utilizando Bootstrap',
+    technology: 'Responsive, Bootstrap, Google API',
+    demo: 'https://cruzmayra.github.io/lab-car-boilerplate/',
+    code: 'https://github.com/CruzMayra/lab-car-boilerplate'
+  }
+];
